a5a31118efaaf21d6139427a53675ea5f2fc496d
Agahnim <gabrielguysoulard@gmail.com>
2026-01-12 10:54:38 +0100

    wip api but need the backend to help

--- src-tauri/src/api/api.rs
+++ src-tauri/src/api/api.rs
@@ -1,3 +1,11 @@
-// pub fn create_university() -> RetType {
-//     todo!();
+use reqwest;
+use serde_json::{json, Value};
+
+// pub async fn create_university() -> Result<Value, reqwest::Error> {
+//     let payload = json!({
+//         "test":"test"
+//     });
+//     let url = "urlquiseraremplie";
+//     let body = reqwest::get(url).await?.json().await?;
+//     Ok(body)
 // }
