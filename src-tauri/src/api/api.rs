d04991e77d5d787455f1ecac2921266f3746f89d
Agahnim <gabrielguysoulard@gmail.com>
2026-01-13 08:57:33 +0100

    add skeleton for rest

--- src-tauri/src/api/api.rs
+++ src-tauri/src/api/api.rs
@@ -9,3 +9,11 @@ pub async fn create_university_request() -> Result<Value, reqwest::Error> {
     let body = reqwest::get(url).await?.json().await?;
     Ok(body)
 }
+pub async fn create_company_request() -> Result<Value, reqwest::Error> {
+    let payload = json!({
+        "test":"test"
+    });
+    let url = "urlquiseraremplie";
+    let body = reqwest::get(url).await?.json().await?;
+    Ok(body)
+}
--- src-tauri/src/commands/commands.rs
+++ src-tauri/src/commands/commands.rs
@@ -1,9 +1,13 @@
-use crate::api::api::create_university_request;
+use crate::api::api::{create_company_request, create_university_request};
 
 #[tauri::command]
 ///We could use a domain like we did in the main app backend
 ///for now get should do the work
 async fn create_university() -> Result<String, reqwest::Error> {
     let body = create_university_request().await?;
+    Ok(body.get("password").unwrap().to_string())
+}
+async fn create_company -> Result<String, reqwest::Error> {
+    let body = create_company_request().await?;
     Ok(body.get("password").unwrap().to_string())
 }
