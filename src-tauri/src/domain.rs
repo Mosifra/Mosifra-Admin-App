f3525db7445aff8421258b44490d8bca9c4d3d5a
Nissyaniss <nissya.proton.me>
2026-01-19 11:51:36 +0100

    attempt to fix get of universities

--- src-tauri/src/domain.rs
+++ src-tauri/src/domain.rs
@@ -1,37 +1,37 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct CreateUniversityResponse {
-    pub success: bool,
-    pub password: String,
+	pub success: bool,
+	pub password: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct CreateCompanyResponse {
-    pub success: bool,
-    pub password: String,
+	pub success: bool,
+	pub password: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct FetchUniversitiesResponse {
-    pub universities: Result<Vec<University>, String>,
+	pub universities: Vec<University>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct FetchCompaniesResponse {
-    pub companies: Result<Vec<Company>, String>,
+	pub companies: Result<Vec<Company>, String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct University {
-    pub login: String,
-    pub mail: String,
-    pub name: String,
+	pub login: String,
+	pub mail: String,
+	pub name: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Company {
-    pub login: String,
-    pub mail: String,
-    pub name: String,
+	pub login: String,
+	pub mail: String,
+	pub name: String,
 }
